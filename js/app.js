01936b634ce29114db8eced0d5b8fd05125958bc
Matthew McDonald <matt.mcodnald21@gmail.com>
2018-06-19 03:24:31 -0500

    feat: bug fixed on timer and added pop up when game is completed
    the timer bug, added to make sure the timer does not start untill the first card was clicked
    
    the pop up, was added to display th amount of time as well as the score when the used has completed the game

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 /*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+ /*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+/*
  * Create a list that holds all of your cards
  */
  const icons = ["fa fa-diamond",
@@ -19,19 +37,12 @@
      "fa fa-bomb",];
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
  const cardContainer = document.querySelector(".deck")
- const bodyContainer = document.querySelector("body")
+ const modalContainer = document.querySelector(".modal")
  let openCards = [];
  let matchedCards = [];
- 
- 
+ let firstClick = true;
+
 // Shuffle function from http://stackoverflow.com/a/2450976
  function shuffle(array) {
     var currentIndex = icons.length, temporaryValue, randomIndex;
@@ -43,7 +54,6 @@
         icons[currentIndex] = icons[randomIndex];
         icons[randomIndex] = temporaryValue;
     }
-
     return array;
  }
 
@@ -52,95 +62,94 @@
  */
  shuffle(icons);
  startGame();
- 
+ over();
+
  /*
  *   Creates the cards
  */
 
-  function startGame(){
- 	 for (let i = 0; i < icons.length; i++) {
-	 	const card = document.createElement('li');
-	 	card.classList.add("card");
-	 	card.innerHTML = ` <i class="${icons[i]}"></i>`;
-	 	cardContainer.appendChild(card);
-	 	clickable(card);
- 	 }	
-  }
+function startGame(){
 
- /*
- *  Make the cards clickable
- */
+ 	for (let i = 0; i < icons.length; i++) {
+	   const card = document.createElement('li');
+	   card.classList.add("card");
+	   card.innerHTML = ` <i class="${icons[i]}"></i>`;
+	   cardContainer.appendChild(card);
+	   clickable(card);
+ 	}	
 
- function clickable(card){
+    /*
+    *  Make the cards clickable
+    */
 
+    function clickable(card){
 
+    /*
+    *   this checks to see if the cards match and what to do if they do
+    */
 
- /*
- *   this checks to see if the cards match and what to do if they do
- */
+        card.addEventListener("click", function(){
+            if (firstClick) {
+            startTimer();
+            firstClick = false;
+          }
+         	const firstCard = this;
+           	const secondCard = openCards[0];
+               	if (openCards.length === 1) {
+                		card.classList.add("open","show", "disable");
+                 		openCards.push("this");
+             		if (firstCard.innerHTML === secondCard.innerHTML) {
+             			firstCard.classList.add("match");
+             			secondCard.classList.add("match");
+             			matchedCards.push(firstCard, secondCard);                   
+             			openCards = [];    		
+                        over();		
+             		} else {
+             			setTimeout(function(){
+                        	firstCard.classList.remove("open", "show", "disable");
+                            secondCard.classList.remove("open", "show","disable");
+             			    openCards = [];
+                		}, 550);
+                 	}
+             	} else {
+             		card.classList.add("open", "show", "disable");
+             		openCards.push(this);
+             	}
+               //Add moves function
+                addMoves();
+        });
+    }
+}
 
-    card.addEventListener("click", function(){
-     	const firstCard = this;
-     	const secondCard = openCards[0];
-     	if (openCards.length === 1) {
-     		card.classList.add("open","show", "disable");
-     		openCards.push("this");
-     			if (firstCard.innerHTML === secondCard.innerHTML) {
-     				firstCard.classList.add("match");
-     				secondCard.classList.add("match");
-     				matchedCards.push(firstCard, secondCard);
-                    
-     				openCards = [];
-     				
-     			} else {
-     				setTimeout(function(){
-     					firstCard.classList.remove("open", "show", "disable");
-     					secondCard.classList.remove("open", "show", "disable");
-
-     					openCards = [];
-     				}, 550);
-     			}
-     	} else {
-     		card.classList.add("open", "show");
-     		openCards.push(this);
-
-     	}
-       //Add moves function
-        addMoves();
-        
-    });
-
- }
-startTimer();
  /*
  * This is to count the moves (per click!).
  */
- const moveContainer = document.querySelector('.moves')
- let moving = 0;
- moveContainer.innerHTML = 0;
- function addMoves(){
-    moving++;
-    moveContainer.innerHTML = moving;
-
-  //Calls the Rating function
-    rating();
+  const moveContainer = document.querySelector('.moves')
+  let moving = 0;
+    moveContainer.innerHTML = 0;
+      function addMoves(){
+         moving++;
+         moveContainer.innerHTML = moving;
      
- }
+       //Calls the Rating function
+         rating();
+          
+      }
  /*
  * Star Rating
  */
- const starContainer = document.querySelector('.stars')
- const star = `<li><i class="fa fa-star"></i></li>`;
- starContainer.innerHTML = star + star + star;
- function rating(){
-    if (moving > 30) {
-        starContainer.innerHTML = star;
-    } else if (moving > 25) {
-        starContainer.innerHTML = star + star;
-    } else {
-        starContainer.innerHTML = star + star + star;
-    }
- }
+  const starContainer = document.querySelector('.stars')
+  const star = `<li><i class="fa fa-star"></i></li>`;
+    starContainer.innerHTML = star + star + star;
+      function rating(){
+        if (moving > 30) {
+            starContainer.innerHTML = star;
+        } else if (moving > 25) {
+            starContainer.innerHTML = star + star;
+        } else {
+            starContainer.innerHTML = star + star + star;
+        }
+  }
        
  /*
  * Restart the game
@@ -151,35 +160,25 @@ startTimer();
     window.location.reload();
  });
 
- /*
- *   Is the game over?
- */
-
- function over(){
-    if (matchedCards.length === icons.length) {
-       
-       stopTimer();
-    } else {
-        console.log("not quite!");
-    }
- }
-
 /*
 *   TIMER OUT-PUT   
 */
 
-function startTimer(){ 
-let timeVar = setInterval(countTimer, 1000);
-}
+ let timeVar;
 
-function stopTimer(){
+ function startTimer(){ 
+    let timeVar = setInterval(countTimer, 1000);
+ }
+
+ function stopTimer(){
     clearInterval(timeVar);
     sec = 0;
     min = 0;
-}
-let totalSec = 0;
+ }
 
-function countTimer() {
+ let totalSec = 0;
+
+ function countTimer() {
     ++totalSec;
     var hour = Math.floor(totalSec /3600);
     var min = Math.floor((totalSec - hour*3600)/60);
@@ -193,41 +192,57 @@ function countTimer() {
         sec = "00";
     }
     document.querySelector('.timer').innerHTML = min + ":" + sec;
-}
+ }
 
- 
-
-
-
-
-/*
-*   custom alert!
-*/
-
-
-
-
-
-
-
-
-
-
-/*
-*
-*/
-
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ /*
+ *   Is the game over?
  */
+
+ function over(){
+    // for ( var i = matchedCards.length; i < icons.length; i++) {
+    //     stopTimer();
+    //     display();
+    // }
+    if (matchedCards.length === icons.length) {
+       
+       console.log("alert!!!");
+       stopTimer();
+      
+      display();
+      
+    }
+ }
+
+ function display(){
+
+    document.getElementById('modal').style.display = "block";
+
+/*
+*   making this easier to read
+*/
+    let time = document.getElementById('time');
+    let timer = document.querySelector('.timer');
+    let star = document.getElementById('stars');
+    let stars = document.querySelector('.stars');
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    document.getElementById('header').innerHTML = "CONGRATULATIONS OF FINISHING!!";
+
+     time.innerHTML = "you finished in " + timer.innerHTML + "!";
+    
+     star.innerHTML = stars.innerHTML;
+    
+
+ }
+
+ function playAgain(){
+    const again = document.querySelector('.again')
+    
+    again.addEventListener("click", function(){
+        window.location.reload();
+    })
+ }
